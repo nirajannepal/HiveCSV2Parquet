bb2328eed4935f8b4a9abde6255674e421902d12
Nirajan Nepal <ni_210@hotmail.com>
2019-02-05 16:05:46 -0500

    Add files via upload

---
+++ athenaCreateTable.sql
@@ -0,0 +1,33 @@
+create external table ontimeStaging (
+  Year INT,
+  Month INT,
+  DayofMonth INT,
+  DayOfWeek INT,
+  DepTime  INT,
+  CRSDepTime INT,
+  ArrTime INT,
+  CRSArrTime INT,
+  UniqueCarrier STRING,
+  FlightNum INT,
+  TailNum STRING,
+  ActualElapsedTime INT,
+  CRSElapsedTime INT,
+  AirTime INT,
+  ArrDelay INT,
+  DepDelay INT,
+  Origin STRING,
+  Dest STRING,
+  Distance INT,
+  TaxiIn INT,
+  TaxiOut INT,
+  Cancelled INT,
+  CancellationCode STRING,
+  Diverted STRING,
+  CarrierDelay INT,
+  WeatherDelay INT,
+  NASDelay INT,
+  SecurityDelay INT,
+  LateAircraftDelay INT
+) STORED AS PARQUET 
+LOCATION 's3://jack-hurley-talend-bucket/nirajan/hive/airline-parquet-snappy/' 
+tblproperties ("parquet.compress"="SNAPPY");
